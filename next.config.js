3d7a4df4ceb11e852f690e3dbb74d82d708b819f
Farleir <farleir@gmail.com>
2022-09-19 14:29:43 -0300

    Test

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true
 };
 
-module.exports {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-};
 
 module.exports = nextConfig;
 
