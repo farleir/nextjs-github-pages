577534583ecc78fdeeae1ee92fe6ea051c2b9099
Farleir <farleir@gmail.com>
2022-09-19 14:04:36 -0300

    Ajust Module

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   reactStrictMode: true
 };
+module.exports = {
+  basePath: '/nextjs-github-pages',
+  assetPrefix: '/nextjs-github-pages'
+}
 
 module.exports = nextConfig;
 
-module.exports = {
-  basePath: '/nextjs-github-pages',
-    assetPrefix: '/nextjs-github-pages'
-}
+
 
